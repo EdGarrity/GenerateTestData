02dedf4032cd5dc301f58e454d265ca8f59be3fa
Ed Garrity <30885192+EdGarrity@users.noreply.github.com>
2022-12-26 16:45:40 -0500

    WIP

--- generate_stock_data.py
+++ generate_stock_data.py
@@ -12,7 +12,5 @@ if __name__ == '__main__':
     test_data = pd.DataFrame(columns=['Stock', 'Date', 'Key', 'Value'])
 
     stock_data = src.stock_data.get()
-    stock_data = src.calculate_ma.calculate_simple_moving_average(stock_data, '50_day_ma', 50)
-    stock_data = src.calculate_ma.calculate_simple_moving_average(stock_data, '200_day_ma', 200)
     test_data = src.kv_collection.load_stock_data(stock_data, test_data)
     src.kv_collection.save_to_sql(test_data)
--- src/calculate_ma.py
+++ src/calculate_ma.py
@@ -4,7 +4,7 @@
 
 import pandas as pd
 
-def calculate_simple_moving_average(stock_data, name, period):
+def calculate_simple_moving_average(stock_data, test_data, name, period):
     """ Generates the simple moving averages.  Calculates the average of a range of prices by the number of periods within that range.
 
      Args:
@@ -14,4 +14,17 @@ def calculate_simple_moving_average(stock_data, name, period):
     """
     stock_data[name] = stock_data['Norm_Adj_Close'].rolling(period).mean()
 
-    return stock_data
+    records = []
+    
+    for index, row in stock_data.iterrows():
+        # calculate SMA
+        new_record = [row['Stock'], row.name, name, row['Norm_Adj_Open']]
+        records.append(new_record)
+
+    return test_data
+
+def generate_technical_indicators(stock_data, test_data):
+    test_data = calculate_simple_moving_average(stock_data, test_data, '50_day_ma', 50)
+    test_data = calculate_simple_moving_average(stock_data, test_data, '200_day_ma', 200)
+
+    return test_data

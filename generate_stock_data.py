29a398ee8c145a85f85370b08ae2656fc1d85f51
Ed Garrity <30885192+EdGarrity@users.noreply.github.com>
2023-08-01 13:05:34 -0400

    add_trading_days_since_start_of_year()

--- generate_stock_data.py
+++ generate_stock_data.py
@@ -16,6 +16,7 @@ if __name__ == '__main__':
     stock_data = src.stock_data.get()
     stock_data = src.technical_analysis.generate(stock_data)
     stock_data = src.temporal_data.add_trading_days_in_year(stock_data)
+    stock_data = src.temporal_data.add_trading_days_since_start_of_year(stock_data)
     test_data = src.kv_collection.load_stock_data(stock_data, test_data)
     test_data = src.add_gdp_data.add_gdp_data(test_data, 'gdp_data.csv')
     test_data = src.add_cpi_data.add_cpi_data(test_data, 'CPILFENS.csv')
@@ -23,99 +24,25 @@ if __name__ == '__main__':
 
     print(stock_data)
     
-# import yfinance as yf
-# import pandas as pd
 
-# def load_index_data(index_symbols, start_date, end_date):
-#     """
-#     This function downloads stock data and normalizes the data.
-#     :param index_symbols: list of index symbols
-#     :param start_date: start date
-#     :param end_date: end date
-#     :return: dataframe
-#     """
-#     # download data
-#     data = pd.DataFrame()
-#     for stock in index_symbols:
-#         ticker_data = yf.download(stock, start=start_date, end=end_date)
-#         ticker_data['Symbol'] = stock
-#         data = pd.concat([data, ticker_data], axis=0)
+# def add_trading_days_since_start_of_year(stock_data: pd.DataFrame) -> pd.DataFrame:
+#     # Step 1: Extract the year from the dates
+#     stock_data['Year'] = pd.to_datetime(stock_data.index).year
 
-#     # Set the DataFrame index to be the row names (date)
-#     data.index.name = 'Date'
+#     # Step 2: Group by 'Symbol' and 'Year' and calculate the number of trading days since the start of the year
+#     stock_data['TradingDaysSinceStartOfYear'] = stock_data.groupby(['Symbol', 'Year'])['Adj Close'].apply(lambda x: x.notnull().cumsum())
 
-#     # adjust all OHLC using the row names (date)
-#     data['Adj_Open'] = data['Open'] / data['Close'] * data['Adj Close']
-#     data['Adj_High'] = data['High'] / data['Close'] * data['Adj Close']
-#     data['Adj_Low'] = data['Low'] / data['Close'] * data['Adj Close']
-#     data['Adj_Volume'] = data['Volume'] / data['Adj Close'] * data['Close']
-
-#     for stock in index_symbols:
-#         mask = data['Symbol'] == stock
-
-#         # The normalization calculations now use the row names (date) instead of 'Date' column
-#         data.loc[mask, 'Norm_Adj_Close']  \
-#             = (data.loc[mask, 'Adj Close'] - data.loc[mask, 'Adj_Low'].min()) \
-#             / (data.loc[mask, 'Adj_High'].max() - data.loc[mask, 'Adj_Low'].min())
-
-#         data.loc[mask, 'Norm_Adj_High']  \
-#             = (data.loc[mask, 'Adj_High'] - data.loc[mask, 'Adj_Low'].min()) \
-#             / (data.loc[mask, 'Adj_High'].max() - data.loc[mask, 'Adj_Low'].min())
-
-#         data.loc[mask, 'Norm_Adj_Low']  \
-#             = (data.loc[mask, 'Adj_Low'] - data.loc[mask, 'Adj_Low'].min()) \
-#             / (data.loc[mask, 'Adj_High'].max() - data.loc[mask, 'Adj_Low'].min())
-
-#         data.loc[mask, 'Norm_Adj_Open']  \
-#             = (data.loc[mask, 'Adj_Open'] - data.loc[mask, 'Adj_Low'].min()) \
-#             / (data.loc[mask, 'Adj_High'].max() - data.loc[mask, 'Adj_Low'].min())
-
-#         data.loc[mask, 'Norm_Adj_Volume']  \
-#             = (data.loc[mask, 'Adj_Volume'] - data.loc[mask, 'Adj_Volume'].min()) \
-#             / (data.loc[mask, 'Adj_Volume'].max() - data.loc[mask, 'Adj_Volume'].min())
-
-#     return data
-
-# def add_trading_days_in_year(stock_data: pd.DataFrame) -> pd.DataFrame:
-#     """
-#     This function calculates the `TradingDaysInYear` for each stock in the DataFrame.
-
-#     Parameters:
-#         stock_data (pd.DataFrame): A DataFrame with stock data.
-
-#     Returns:
-#         pd.DataFrame: The DataFrame with the `TradingDaysInYear` field added.
-#     """
-#     # Group data by stock symbol and year
-#     grouped_data = stock_data.groupby([stock_data.index.year, 'Symbol'])
-
-#     # Calculate TradingDaysInYear for each stock and year
-#     stock_data['TradingDaysInYear'] = grouped_data['Adj Close'].transform(lambda x: x.notnull().sum())
+#     # Drop the 'Year' column as it is no longer needed
+#     stock_data.drop(columns=['Year'], inplace=True)
 
 #     return stock_data
 
-# def add_stock_temporal_data() -> pd.DataFrame:
-#     """
-#     This program downloads stock data and adds the `TradingDaysInYear` field.
-
-#     For each record in the downloaded stock data, add the following field:
-#     - TradingDaysInYear: Calculate the number of trading days in the year for each stock.
-
-#     Parameters:
-#         None
-
-#     Returns:
-#         pd.DataFrame: A DataFrame with the stock data including the added temporal-related information.
-#     """
-#     # Download data
-#     stock_data = load_index_data(['AAPL', 'FXAIX'], '2019-01-01', '2021-12-31')
-
-#     # Add TradingDaysInYear field
-#     stock_data = add_trading_days_in_year(stock_data)
-
-#     # Return stock data with additional temporal-related information
-#     return stock_data
-
-# if __name__ == '__main__':
-#     stock_data_2 = add_stock_temporal_data()
-#     print(stock_data_2)
+# # Test Example
+# stock_data = pd.DataFrame({
+#     'Symbol':    ['AAPL',       'AAPL',       'AAPL',       'AAPL',       'AAPL',       'GOOGL',      'GOOGL',      'GOOGL',      'GOOGL',      'GOOGL'],
+#     'Date':      ['2022-01-03', '2022-01-04', '2022-01-05', '2022-01-06', '2022-01-07', '2022-01-03', '2022-01-04', '2022-01-05', '2023-01-02', '2023-01-03'],
+#     'Adj Close': [ 180.05,       182.21,       None,         182.22,       182.21,       3100.0,       3095.5,       3134.0,       3144.00,      3144.00]
+# })
+# stock_data.set_index('Date', inplace=True)
+# result = add_trading_days_since_start_of_year(stock_data)
+# print(result)

95309e83ed955686191130f9a013de0b961c4721
Ed Garrity <30885192+EdGarrity@users.noreply.github.com>
2023-08-02 19:21:56 -0400

    WIP

--- generate_stock_data.py
+++ generate_stock_data.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     test_data = pd.DataFrame(columns=['Stock', 'Date', 'Key', 'Value'])
 
     stock_data = src.stock_data.get()
-    # stock_data = src.technical_analysis.generate(stock_data)
+    stock_data = src.technical_analysis.generate(stock_data)
     stock_data = src.temporal_data.add_trading_days_in_year(stock_data)
     stock_data = src.temporal_data.add_trading_days_since_start_of_year(stock_data)
     stock_data = src.temporal_data.add_trading_days_left_in_year(stock_data)
@@ -25,6 +25,7 @@ if __name__ == '__main__':
     test_data = src.kv_collection.load_stock_data(stock_data, test_data)
     test_data = src.add_gdp_data.add_gdp_data(test_data, 'gdp_data.csv')
     test_data = src.add_cpi_data.add_cpi_data(test_data, 'CPILFENS.csv')
-    # src.kv_collection.save_to_sql(test_data)
+
+    src.kv_collection.save_to_sql(test_data)
 
     print(stock_data)
